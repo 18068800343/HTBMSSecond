a0d5816566657cb6df9ea8a2615bc55b5304c847
夏宁 <1042449477@qq.com>
2019-08-05 15:33:20 +0800

    xn

---
+++ HTBMSSecond/src/hs/bm/vo/PassChkVo.java
@@ -0,0 +1,18 @@
+package hs.bm.vo;
+
+public class PassChkVo {
+	private String passId;
+	private String chkId;
+	public String getChkId() {
+		return chkId;
+	}
+	public void setChkId(String chkId) {
+		this.chkId = chkId;
+	}
+	public String getPassId() {
+		return passId;
+	}
+	public void setPassId(String passId) {
+		this.passId = passId;
+	}
+}
